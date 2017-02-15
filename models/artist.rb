7128ebd7cc094d8e9eeb890b24056ff816c45751
Ewen Carr <ewencarr@hotmail.com>
2017-02-15 15:22:41 +0000

    artist save

--- models/artist.rb
+++ models/artist.rb
@@ -1,1 +1,16 @@
 require_relative ('../db/sql_runner.rb')
+
+class Artists
+  attr_reader :id
+  attr_accessor :name
+  def initialize(options)
+    @id = options['id'].to_i
+    @name = options['name']
+  end
+
+  def save
+    sql = "INSERT INTO artists(name) VALUES ('#{@name}')"
+    @id = SqlRunner.run(sql)[0]['id'].to_i
+  end
+  
+end
