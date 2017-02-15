6300c148b7fd5e065c55753e432a2c1a876bdd67
Ewen Carr <ewencarr@hotmail.com>
2017-02-15 15:37:36 +0000

    added save method to album

--- models/album.rb
+++ models/album.rb
@@ -1,2 +1,20 @@
 require_relative ('../db/sql_runner.rb')
 
+class Album
+
+  attr_reader :id, :artist_id
+  attr_accessor :title, :genre
+
+  def initialize(options)
+    @id = options['id'].to_i
+    @artist_id = options['artist_id'].to_i
+    @title = options['title']
+    @genre = options['genre']
+  end
+
+  def save
+    sql = "INSERT INTO albums (artist_id, title, genre) VALUES (#{@artist_id}, '#{@title}', '#{@genre}') RETURNING *"
+    @id = SqlRunner(sql)[0]['id'].to_i
+  end
+
+end
